607f19357fee9382745b781e476cbe3cb13df2d4
Mehrab Hafiz <mehrabb.hafiz@gmail.com>
2025-08-30 12:27:40 -0400

    spring initializer

---
+++ src/main/java/com/mehrab/paystream/PaystreamApplication.java
@@ -0,0 +1,13 @@
+package com.mehrab.paystream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehrab/paystream/PaystreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehrab.paystream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
