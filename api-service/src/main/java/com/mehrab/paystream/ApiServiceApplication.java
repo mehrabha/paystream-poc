bca9c1bcce0549807f0cac76deab2c9453c9cf8c
Mehrab Hafiz <mehrabb.hafiz@gmail.com>
2025-08-30 13:50:00 -0400

    child module setup

--- api-service/src/main/java/com/mehrab/paystream/ApiServiceApplication.java
+++ api-service/src/main/java/com/mehrab/paystream/ApiServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaystreamApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PaystreamApplication.class, args);
-	}
-
+public class ApiServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServiceApplication.class, args);
+    }
 }
