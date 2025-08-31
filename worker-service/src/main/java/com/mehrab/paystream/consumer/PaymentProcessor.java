4d71d512d7df0c08f13d35109499aaa1d79f9056
Mehrab Hafiz <mehrabb.hafiz@gmail.com>
2025-08-30 22:14:26 -0400

    consumer setup

--- worker-service/src/main/java/com/mehrab/paystream/consumer/PaymentProcessor.java
+++ worker-service/src/main/java/com/mehrab/paystream/consumer/PaymentProcessor.java
@@ -1,9 +1,15 @@
 package com.mehrab.paystream.consumer;
 
+import java.time.Instant;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import com.mehrab.paystream.avro.Payment;
+import com.mehrab.paystream.model.PaymentEntity;
 import com.mehrab.paystream.repository.PaymentRepository;
 
 
@@ -14,4 +20,20 @@ public class PaymentProcessor {
 
     @Autowired
     private PaymentRepository paymentRepository;
+
+    @KafkaListener(topics = "payments")
+    public void onEvent(ConsumerRecord<String, Payment> message) {
+        Payment paymentData = message.value();
+
+        PaymentEntity entry = new PaymentEntity();
+
+        entry.setId(paymentData.getTransactionId());
+        entry.setUserId(paymentData.getUserId());
+        entry.setAmount(paymentData.getAmount());
+        entry.setCurrency(paymentData.getCurrency());
+        entry.setTimestamp(Instant.now());
+        entry.setStatus("POSTED");
+
+        paymentRepository.save(entry);
+    }
 }
